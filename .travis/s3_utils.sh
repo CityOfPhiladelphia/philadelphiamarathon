8a6b3fcd79dfd3c6c2c55588752348dc5f6ac081
andy.rothwell <rothwell.andy@gmail.com>
2017-09-22 10:26:23 -0400

    changes travis settings

--- .travis/s3_utils.sh
+++ .travis/s3_utils.sh
@@ -6,9 +6,9 @@ get_s3_bucket() {
   # if this isn't a tag build and the branch is something other than master...
   # (tag builds always get pushed to master, and non-master branches get pushed
   # to their own bucket)
-  if [ -z "$TRAVIS_TAG" ] && [ $TRAVIS_BRANCH != "master" ]; then
-    S3_BUCKET+="-$TRAVIS_BRANCH"
-  fi
+  # if [ -z "$TRAVIS_TAG" ] && [ $TRAVIS_BRANCH != "master" ]; then
+  #   S3_BUCKET+="-$TRAVIS_BRANCH"
+  # fi
   echo $S3_BUCKET
   return 0
 }
